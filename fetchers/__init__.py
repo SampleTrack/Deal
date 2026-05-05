2760e5060f8a60acb882e7a71f6c71f00205aeb6
SampleTrack <FirstSample@proton.me>
2026-05-05 23:50:38 +0530

    Create __init__.py

---
+++ fetchers/__init__.py
@@ -0,0 +1,5 @@
+from fetchers.amazon import AmazonFetcher
+from fetchers.flipkart import FlipkartFetcher
+from fetchers.nykaa import NykaaFetcher
+
+__all__ = ["AmazonFetcher", "FlipkartFetcher", "NykaaFetcher"]
